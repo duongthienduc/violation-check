4370ebe078cd7731ba3241354b90222eaa32cdd0
Duc Duong <duc.duong@codeenginestudio.com>
2017-08-02 21:00:07 +0700

    Refine webpack configs

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,25 +1,3 @@
-var webpack = require('webpack');
-var path = require('path');
-
-var BUILD_DIR = path.resolve(__dirname, 'public');
-var APP_DIR = path.resolve(__dirname, 'src');
-
-var config = {
-    entry: APP_DIR + '/index.jsx',
-    output: {
-        path: BUILD_DIR,
-        filename: 'bundle.js'
-    },
-    module : {
-        loaders : [
-            {
-                test : /\.jsx?/,
-                include : APP_DIR,
-                loader : 'babel'
-            }
-        ]
-    }, 
-    watch: true
-};
-
-module.exports = config;
+module.exports = function(env) {
+  return require(`./webpack.config.${env}.js`)
+}
---
+++ react/webpack.config.prod.js
@@ -0,0 +1,51 @@
+var webpack = require('webpack');
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var WebpackJsObfuscator = require('webpack-js-obfuscator');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module : {
+        loaders : [
+            {
+                test : /\.jsx?/,
+                include : APP_DIR,
+                loader : 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            mangle: {
+                screw_ie8: true,
+                keep_fnames: true
+            },
+            compress: {
+                screw_ie8: true
+            },
+            comments: false
+        })
+    ],
+    resolve: {
+        extensions: [ '.js', '.jsx' ]
+    }
+};
+
+module.exports = config;
